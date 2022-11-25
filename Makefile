5aedc2437c22e7b515c63a75511b3daff94f39e2
A. J. Wright <ajw@toadking.org>
2022-11-25 16:58:38 -0500

    Tuned visual effects to make it look like a firely.

--- Makefile
+++ Makefile
@@ -4,8 +4,9 @@
 # https://docs.circuitpython.org/en/latest/docs/workflows.html#get
 #
 
-# Generic name on the network
-CPURL=http://circuitpython.local
+# URL to access circuitpython hardware
+#   Use CPURL environment variable if its set
+CPURL := $(if $(CPURL),$(CPURL),http://circuitpython.local)
 
 # the web api login password
 CIRCUITPY_WEB_API_PASSWORD=REDACTED_FOR_GITHUB
@@ -14,18 +15,20 @@ CIRCUITPY_WEB_API_PASSWORD=REDACTED_FOR_GITHUB
 VERBOSE=-v
 
 # No config below this line
-all: push .gitignore
+all: install .gitignore
 
-push: boot.py code.py
+install: boot.py code.py
 	date -r code.py "+__version__ = %'%Y-%m-%d %H:%M:%S%'" > version.py
 	curl $(VERBOSE) -u :$(CIRCUITPY_WEB_API_PASSWORD) --location --location-trusted \
 		--upload-file version.py $(CPURL)/fs/version.py \
 		--upload-file boot.py $(CPURL)/fs/boot.py \
 		--upload-file code.py $(CPURL)/fs/code.py
 
-push-lib: downloads downloads/bundle/lib/neopixel.mpy
+install-lib: downloads downloads/bundle/lib/neopixel.mpy downloads/bundle/lib/adafruit_fancyled/adafruit_fancyled.mpy
 	cd downloads/bundle/lib && \
 	curl $(VERBOSE) -u :$(CIRCUITPY_WEB_API_PASSWORD) --location --location-trusted \
+		--upload-file adafruit_fancyled/adafruit_fancyled.mpy $(CPURL)/fs/lib/adafruit_fancyled/adafruit_fancyled.mpy \
+		--upload-file adafruit_fancyled/__init__.py $(CPURL)/fs/lib/adafruit_fancyled/__init__.py \
 		--upload-file neopixel.mpy $(CPURL)/fs/lib/neopixel.mpy
 
 get-cp-info:
@@ -37,14 +40,15 @@ get-cp-info:
 .gitignore:
 	curl https://www.toptal.com/developers/gitignore/api/python,circuitpython,git,virtualenv,macos,vim,pycharm -o .gitignore
 	printf "\n# ignore the downloads directory\ndownloads\n" >> .gitignore
-	printf "\n# ignore version.py that updates each push\nversion.py\n" >> .gitignore
+	printf "\n# ignore version.py that updates each install\nversion.py\n" >> .gitignore
 
 downloads:
 	test -d downloads || mkdir downloads
 	cd downloads && curl --location --progress-bar \
+		-O https://downloads.circuitpython.org/bin/feather_m0_express/en_US/adafruit-circuitpython-feather_m0_express-en_US-7.3.3.uf2 \
+		-O https://downloads.circuitpython.org/bin/adafruit_feather_huzzah32/en_US/adafruit-circuitpython-adafruit_feather_huzzah32-en_US-8.0.0-beta.4.bin \
 		-O https://github.com/adafruit/Adafruit_CircuitPython_Bundle/releases/download/20221122/adafruit-circuitpython-bundle-8.x-mpy-20221122.zip \
-		-O https://downloads.circuitpython.org/bin/adafruit_feather_huzzah32/en_US/adafruit-circuitpython-adafruit_feather_huzzah32-en_US-8.0.0-beta.4.bin && \
-		unzip adafruit-circuitpython-bundle-8.x-mpy-20221122.zip && \
+		&& unzip adafruit-circuitpython-bundle-8.x-mpy-20221122.zip && \
 		ln -s adafruit-circuitpython-bundle-8.x-mpy-20221122 bundle
 
 clean:
--- code.py
+++ code.py
@@ -1,25 +1,113 @@
 #!/usr/bin/env python3
 # code.py
+import atexit
+import random
+import sys
 import time
+
+import adafruit_fancyled.adafruit_fancyled as fancyled
 import board
-import neopixel
-import version
 import digitalio
+import neopixel
+import supervisor
+
+import version
+
+# Config variables
+strand_pin = board.D21
+strand_length: int = 50
+strand_brightness: float = 0.1
+brightness_levels = (0.25, 0.3, 0.15)  # balance the colors better so white doesn't appear blue-tinged
+
+# Length of various timers
+# Floats are in seconds
+# Integers are in microseconds (1000ths)
+move_frequency_min: int = 500
+move_frequency_max: int = 5000
+strand_on_time: float = 0.1
+strand_min_off_time: int = 1000
+strand_max_off_time: int = 2000
+
+# No user config below this point
 
 print("Ƹ̵̡Ӝ̵̨̄Ʒ firefly - github.com/algrym/firefly/code.py -", version.__version__)
+print(f" - Adafruit FancyLed v{fancyled.__version__}")
 
-FIREFLY_GREEN = (0, 255, 175)
+# Color constants
+RED = fancyled.gamma_adjust(fancyled.CRGB(255, 0, 0), brightness=brightness_levels).pack()
+ORANGE = fancyled.gamma_adjust(fancyled.CRGB(255, 165, 0), brightness=brightness_levels).pack()
+YELLOW = fancyled.gamma_adjust(fancyled.CRGB(255, 255, 0), brightness=brightness_levels).pack()
+GREEN = fancyled.gamma_adjust(fancyled.CRGB(0, 255, 0), brightness=brightness_levels).pack()
+FIREFLY_GREEN = fancyled.gamma_adjust(fancyled.CRGB(0, 255, 175), brightness=brightness_levels).pack()
+BLUE = fancyled.gamma_adjust(fancyled.CRGB(0, 0, 255), brightness=brightness_levels).pack()
+PURPLE = fancyled.gamma_adjust(fancyled.CRGB(128, 0, 128), brightness=brightness_levels).pack()
+WHITE = fancyled.gamma_adjust(fancyled.CRGB(255, 255, 255), brightness=brightness_levels).pack()
 OFF = (0, 0, 0)
 
+color_wheel = [RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE]
+
+
+def all_off():
+    # callback to turn everything off on exit
+    print(' - Exiting: all pixels off.')
+    strand_pixels.fill(OFF)
+    sys.exit(0)
+
+
+# turn everything off on exit
+atexit.register(all_off)
+
+# setup onboard LED
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
+# setup strand NEOPIXELs
+print(f" - NeoPixel strand size {strand_length} on {strand_pin}")
+strand_pixels = neopixel.NeoPixel(strand_pin, strand_length)
+# brightness=strand_brightness)
+
+next_move_clock = next_strand_clock = 0
+strand_direction = 1
+strand_cursor = 0
+
+print(' - Running LED test.')
+for c in color_wheel:
+    strand_pixels.fill(c)
+    time.sleep(strand_on_time)
+strand_pixels.fill(OFF)
+
+print(' - Entering main event loop.')
 while True:
-    led.value = 0.1
+    clock = supervisor.ticks_ms()
+
+    # Adjust location if clock is expired
+    if clock > next_move_clock:
+        next_move_clock = clock + random.randrange(move_frequency_min, move_frequency_max)
+
+        # Blank all the pixels
+        strand_pixels.fill(OFF)
+
+        # Bounce if the cursor is at the end of the strand
+        if strand_cursor == 0:
+            strand_direction = 1
+        elif strand_cursor >= (len(strand_pixels) - 1):
+            strand_direction = -1
+
+        # Move the firefly
+        strand_cursor = (strand_cursor + strand_direction) % len(strand_pixels)
+
+    # Flicker pixels if clock is expired
+    if clock > next_strand_clock:
+        next_strand_clock = clock + random.randrange(strand_min_off_time, strand_max_off_time)
+
+        strand_pixels[strand_cursor] = FIREFLY_GREEN
+        time.sleep(strand_on_time)
+        strand_pixels[strand_cursor] = OFF
+        time.sleep(3 * strand_on_time)
+        strand_pixels[strand_cursor] = FIREFLY_GREEN
+        time.sleep(strand_on_time)
+        strand_pixels[strand_cursor] = OFF
+
+    # Increment heartbeat LED
+    led.value = clock % 50
     time.sleep(0.1)
-    led.value = False
-    time.sleep(0.1)
-    led.value = 0.1
-    time.sleep(0.1)
-    led.value = False
-    time.sleep(0.8)
