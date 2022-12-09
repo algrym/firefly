c2f24558f1d491d184a2600f2c4919b653003873
A. J. Wright <ajw@toadking.org>
2022-12-09 16:56:45 -0500

    Revert "Updated boot.py to connect to wifi"
    This reverts commit 24c4f877bac42a417a5623128f384db24d77e1c8.

--- boot.py
+++ boot.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python3
 # boot.py
-import os
-import ipaddress
 import wifi
-import socketpool
 
-print("Connecting to WiFi . . .")
-
-#  connect to your SSID
-wifi.radio.connect(os.getenv('WIFI_SSID'), os.getenv('WIFI_PASSWORD'))
-
-print(f"Connected to WiFi: {os.getenv('WIFI_SSID')}")
-
-pool = socketpool.SocketPool(wifi.radio)
-
-#  prints MAC address to REPL
-print("My MAC addr:", [hex(i) for i in wifi.radio.mac_address])
-
-#  prints IP address to REPL
-print("My IP address is", wifi.radio.ipv4_address)
-
-#  pings Google
-ipv4 = ipaddress.ip_address("8.8.4.4")
-print("Ping google.com: %f ms" % (wifi.radio.ping(ipv4)*1000))
+print("# My MAC addr: %02X:%02X:%02X:%02X:%02X:%02X" % tuple(wifi.radio.mac_address))
+print("# My IP address is", wifi.radio.ipv4_address)
