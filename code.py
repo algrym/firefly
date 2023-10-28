7cdba877d18b0a6716decb89814ad3a540f0dca5
A. J. Wright <ajw@toadking.org>
2023-10-27 20:48:24 -0400

    Premature optimization

--- code.py
+++ code.py
@@ -59,7 +59,7 @@ def all_off():
     watch_dog.deinit()
     if supervisor.runtime.serial_connected:
         print(' - Exiting: setting all pixels off.')
-    strand_pixels.fill(OFF)
+    neopixel.NeoPixel(STRAND_PIN, STRAND_LENGTH).fill(OFF)
     supervisor.reload()
 
 
@@ -73,88 +73,108 @@ if supervisor.runtime.serial_connected:
 # turn everything off on exit
 atexit.register(all_off)
 
-# setup onboard LED
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
 
-# setup Neopixel strand
-if supervisor.runtime.serial_connected:
-    print(f' - NeoPixel strand size {STRAND_LENGTH} on {STRAND_PIN}')
-strand_pixels = neopixel.NeoPixel(STRAND_PIN, STRAND_LENGTH)
+def main_event_loop():
+    """For CircuitPython optimization, this is just the primary loop
+    of the script tucked into one big fat function.
+    We also pull some global vars in so now they're function-local.
 
-# Initialize counters and clocks
-next_move_clock: int = 0
-next_strand_clock: int = 0
-next_stat_clock: int = supervisor.ticks_ms() + 10000
-strand_direction: int = 1
-strand_cursor: int = random.randrange(0, len(strand_pixels))
-start_time: int = int(time.time())
-last_loop_time: int = start_time
-loop_count: int = 0
-strand_blink_count: int = 0
+    It turns out that fetching global vars is expensive.
 
-if supervisor.runtime.serial_connected:
-    print(' - Running LED test.')
-for c in color_wheel:
-    strand_pixels.fill(c)
-    watch_dog.feed()
-    time.sleep(STRAND_INIT_DELAY)
-strand_pixels.fill(OFF)
+    Before:
+     - Running 271s at 11340.1 loops/second
 
-if supervisor.runtime.serial_connected:
-    print(' - Entering main event loop.')
-while True:
-    clock = supervisor.ticks_ms()
-    watch_dog.feed()
-    led.value = not led.value
-    loop_count += 1
+    After:
+     - Running 320s at 39404.7 loops/second
+    """
 
-    # Print the average runs per second ever 10secs
-    if clock > next_stat_clock:
-        next_stat_clock: int = clock + 10000
-        if supervisor.runtime.serial_connected:
-            print(
-                f" - Running {time.time() - start_time}s at {loop_count / (time.time() - last_loop_time)} loops/second")
-        loop_count: int = 0
-        last_loop_time = int(time.time())
+    # setup onboard LED
+    led = digitalio.DigitalInOut(board.LED)
+    led.direction = digitalio.Direction.OUTPUT
 
-    # Flicker pixels if clock is expired
-    if clock > next_strand_clock:
-        # If there's still blink pattern to use, do so.
-        #  Otherwise reset
-        if strand_blink_count < len(STRAND_BLINK_PATTERN):
-            next_strand_clock = clock + STRAND_BLINK_PATTERN[strand_blink_count]
-            strand_blink_count += 1
-        else:
-            next_strand_clock = clock + random.randrange(STRAND_MIN_OFF_TIME, STRAND_MAX_OFF_TIME)
+    # setup Neopixel strand
+    if supervisor.runtime.serial_connected:
+        print(f' - NeoPixel strand size {STRAND_LENGTH} on {STRAND_PIN}')
+    strand_pixels = neopixel.NeoPixel(STRAND_PIN, STRAND_LENGTH)
+
+    if supervisor.runtime.serial_connected:
+        print(' - Running LED test.')
+    for c in color_wheel:
+        strand_pixels.fill(c)
+        watch_dog.feed()
+        time.sleep(STRAND_INIT_DELAY)
+    strand_pixels.fill(OFF)
+
+    # Initialize counters and clocks
+    next_move_clock: int = 0
+    next_strand_clock: int = 0
+    next_stat_clock: int = supervisor.ticks_ms() + 10000
+    strand_direction: int = 1
+    strand_cursor: int = random.randrange(0, len(strand_pixels))
+    start_time: int = int(time.time())
+    last_loop_time: int = start_time
+    loop_count: int = 0
+    strand_blink_count: int = 0
+
+    if supervisor.runtime.serial_connected:
+        print(' - Entering main event loop.')
+
+    while True:
+        clock = supervisor.ticks_ms()
+        watch_dog.feed()
+        led.value = not led.value
+        loop_count += 1
+
+        # Print the average runs per second ever 10secs
+        if clock > next_stat_clock:
+            next_stat_clock: int = clock + 10000
+            if supervisor.runtime.serial_connected:
+                print(
+                    f" - Running {time.time() - start_time}s at {loop_count / (time.time() - last_loop_time)} loops/second")
+            loop_count: int = 0
+            last_loop_time = int(time.time())
+
+        # Flicker pixels if clock is expired
+        if clock > next_strand_clock:
+            # If there's still blink pattern to use, do so.
+            #  Otherwise reset
+            if strand_blink_count < len(STRAND_BLINK_PATTERN):
+                next_strand_clock = clock + STRAND_BLINK_PATTERN[strand_blink_count]
+                strand_blink_count += 1
+            else:
+                next_strand_clock = clock + random.randrange(STRAND_MIN_OFF_TIME, STRAND_MAX_OFF_TIME)
+                strand_blink_count = 0
+
+            # Handling blinking the light
+            if strand_pixels[strand_cursor] == OFF:
+                strand_pixels[strand_cursor] = FIREFLY_GREEN
+            else:
+                strand_pixels[strand_cursor] = OFF
+            continue  # Don't move the firefly while in the flash pattern
+
+        # Adjust location if clock is expired
+        if clock > next_move_clock:
+            next_move_clock = clock + random.randrange(MOVE_FREQUENCY_MIN, MOVE_FREQUENCY_MAX)
+
+            # Blank all the pixels
+            strand_pixels.fill(OFF)
             strand_blink_count = 0
 
-        # Handling blinking the light
-        if strand_pixels[strand_cursor] == OFF:
-            strand_pixels[strand_cursor] = FIREFLY_GREEN
-        else:
-            strand_pixels[strand_cursor] = OFF
-        continue  # Don't move the firefly while in the flash pattern
+            # Decide if we switch direction
+            if random.randrange(0, 2) == 0:
+                strand_direction: int = strand_direction * int(-1)
 
-    # Adjust location if clock is expired
-    if clock > next_move_clock:
-        next_move_clock = clock + random.randrange(MOVE_FREQUENCY_MIN, MOVE_FREQUENCY_MAX)
+            # Bounce if the cursor is at the end of the strand
+            if strand_cursor == 0:
+                strand_direction = 1
+            elif strand_cursor >= (len(strand_pixels) - 1):
+                strand_direction = -1
 
-        # Blank all the pixels
-        strand_pixels.fill(OFF)
-        strand_blink_count = 0
+            # Move the firefly
+            if supervisor.runtime.serial_connected:
+                print(f'   - Moving cursor={strand_cursor} direction={strand_direction} max={len(strand_pixels)}')
+            strand_cursor = (strand_cursor + strand_direction) % len(strand_pixels)
 
-        # Decide if we switch direction
-        if random.randrange(0, 2) == 0:
-            strand_direction: int = strand_direction * int(-1)
 
-        # Bounce if the cursor is at the end of the strand
-        if strand_cursor == 0:
-            strand_direction = 1
-        elif strand_cursor >= (len(strand_pixels) - 1):
-            strand_direction = -1
-
-        # Move the firefly
-        if supervisor.runtime.serial_connected:
-            print(f'   - Moving cursor={strand_cursor} direction={strand_direction} max={len(strand_pixels)}')
-        strand_cursor = (strand_cursor + strand_direction) % len(strand_pixels)
+if __name__ == "__main__":
+    main_event_loop()
